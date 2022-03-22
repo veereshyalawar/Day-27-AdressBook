8e9dc7aba8fa565170e1102f8011bbce0e50252b
veeresh <veereshyalawar2@gmail.com>
2022-03-23 01:57:17 +0530

    added uc5 files

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,4 +1,3 @@
-
 /**
  * importing scanner class to take input from console
  */
@@ -15,16 +14,31 @@ public class AddressBookMain {
 		AddressBook addressBook = new AddressBook();
 		Scanner scanner = new Scanner(System.in);
 		/**
-		 * from addressBook class calling add contact method to edit and delete the
-		 * contacts
+		 * while condition is taken to check the condition is it is true enters to the
+		 * switch case statement
 		 */
-		addressBook.addContact();
-		System.out.println("Enter 0 To Edit The Contact and 1 To Delete The Contact ");
-		int choice = scanner.nextInt();
-		if (choice == 0) {
-			addressBook.editContact();
-		} else {
-			addressBook.deleteContact();
+		while (true) {
+			System.out.println(
+					"Enter \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact \n 4 to exit");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.editContact();
+				break;
+			case 3:
+				addressBook.deleteContact();
+				break;
+			case 4:
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Enter the wrong input");
+				continue;
+			}
+
 		}
 
 	}
